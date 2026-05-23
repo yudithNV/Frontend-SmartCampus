68a1fd412be9b8d2bd380f59f665d77b8f117ed1
fabrisiocondarco <fabrisio.condarco@ucb.edu.bo>
2026-05-23 00:00:26 -0400

    SCRUM-480 FINALIZADO
    SCRUM-480 FINALIZADO

--- src/components/Admin/AdminSidebar.vue
+++ src/components/Admin/AdminSidebar.vue
@@ -98,6 +98,18 @@
         </transition>
       </router-link>
 
+      <router-link to="/admin/sugerencias" class="menu-item" :class="{ active: $route.path.includes('sugerencias') }">
+        <span class="icon">
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+            <path d="M9 11l3 3L22 4"/>
+            <path d="M21 12v7a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h11"/>
+          </svg>
+        </span>
+        <transition name="text-fade">
+          <span v-if="!collapsed">Sugerencias</span>
+        </transition>
+      </router-link>
+
       <router-link to="/admin/configuracion" class="menu-item" :class="{ active: $route.path.includes('configuracion') }">
         <span class="icon">
           <!-- Settings -->
